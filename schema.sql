a15fb3e78fe3790aa5723c6eb472080a4d9c1f20
Ben Kiboma <omayioben@gmail.com>
2022-08-09 23:13:49 +0300

    Create animals table

--- schema.sql
+++ schema.sql
@@ -3,3 +3,12 @@
 CREATE TABLE animals (
     name varchar(100)
 );
+
+CREATE TABLE animals(
+    id INT PRIMARY KEY NOT NULL,
+    name CHAR(50) NOT NULL,
+    date_of_birth DATE NOT NULL,
+    escape_attempts INT,
+    neutered BOOLEAN,
+    weight_kg DECIMAL
+    );
