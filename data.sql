8179a91792a159069228f8ee1de2f7650dafc02d
Ben Kiboma <omayioben@gmail.com>
2022-08-09 23:36:48 +0300

    Write queries

--- data.sql
+++ data.sql
@@ -2,5 +2,5 @@
 
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (1, 'Agumon', 'Feb-3-2020', 0, true, 10.23);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (2, 'Gabumon', 'Nov-15-2018', 2, true, 8);
-INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', 'Jan-07-2021', 1, false, 15.04);
 INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (4, 'Devimon', 'May-12-2017', 5, true, 11);
+INSERT INTO animals (id, name, date_of_birth, escape_attempts, neutered, weight_kg) VALUES (3, 'Pikachu', 'Jan-07-2021', 1, false, 15.04);
